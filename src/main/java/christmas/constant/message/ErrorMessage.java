43582bf35736e3023a8fe398c635bac4ef3a677f
hwan2-99 <thh1233123@gmail.com>
2023-11-09 23:50:32 +0900

    feat: 주문메뉴와 수량입력시 수량은 숫자 1이상만 입력 요구하는 검증기능 구현

--- src/main/java/christmas/constant/message/ErrorMessage.java
+++ src/main/java/christmas/constant/message/ErrorMessage.java
@@ -3,7 +3,8 @@ package christmas.constant.message;
 public enum ErrorMessage {
     ERROR("[ERROR]"),
     OVER_DATE_RANGE_ERROR(ERROR.message + "유효하지 않은 날짜입니다. 다시 입력해 주세요."),
-    INCORRECT_INPUT_ERROR(ERROR.message + "숫자만 입력해야 합니다. 다시 입력해 주세요");
+    INCORRECT_INPUT_ERROR(ERROR.message + "숫자만 입력해야 합니다. 다시 입력해 주세요"),
+    INCORRECT_ORDER_ERROR(ERROR.message + "유효하지 않은 주문입니다. 다시 입력해 주세요.");
 
     private final String message;
 
--- src/main/java/christmas/controller/EventPlanner.java
+++ src/main/java/christmas/controller/EventPlanner.java
@@ -1,22 +1,26 @@
 package christmas.controller;
 
+import christmas.model.OrderItem;
 import christmas.view.InputView;
 import java.util.Map;
 import java.util.Map.Entry;
 
 public class EventPlanner {
     private final InputView inputView = new InputView();
-    public void run(){
+
+    public void run() {
         System.out.println(getVisitDate());
-        Map<String,Integer> orderInfo = getOrderInfo();
-        for (Entry<String, Integer> entry : orderInfo.entrySet()) {
+        OrderItem orderItem = new OrderItem(getOrderInfo());
+        for (Entry<String, Integer> entry : orderItem.getOrderInfo().entrySet()) {
             System.out.println(entry.getKey() + ": " + entry.getValue());
         }
     }
-    private int getVisitDate(){
+
+    private int getVisitDate() {
         return inputView.askVisitDate();
     }
-    private Map<String,Integer> getOrderInfo() {
+
+    private Map<String, Integer> getOrderInfo() {
         return inputView.askOrderMenus();
     }
 }
