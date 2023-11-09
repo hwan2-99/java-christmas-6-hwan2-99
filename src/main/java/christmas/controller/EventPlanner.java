670223e876b31dbd7f2de2dd767de0cbdd24d0b2
hwan2-99 <thh1233123@gmail.com>
2023-11-09 21:21:29 +0900

    feat: 주문메뉴와 수량입력을 위한 안내문구 출력기능 구현

--- src/main/java/christmas/controller/EventPlanner.java
+++ src/main/java/christmas/controller/EventPlanner.java
@@ -1,13 +1,22 @@
 package christmas.controller;
 
 import christmas.view.InputView;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class EventPlanner {
     private final InputView inputView = new InputView();
     public void run(){
         System.out.println(getVisitDate());
+        Map<String,Integer> orderInfo = getOrderInfo();
+        for (Entry<String, Integer> entry : orderInfo.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
     }
     private int getVisitDate(){
         return inputView.askVisitDate();
+    }
+    private Map<String,Integer> getOrderInfo() {
+        return inputView.askOrderMenus();
     }
 }
--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,9 +1,18 @@
 package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import christmas.model.OrderItem;
 import christmas.validator.VisitDateValidation;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import javax.print.DocFlavor.STRING;
 
 public class InputView {
+    private static final String COMMA = ",";
+    private static final String HYPHEN = "-";
     private static final String ASK_VISIT_DATE_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.\n"
             + "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
     private static final String ASK_ORDER_INFO = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
@@ -21,6 +30,18 @@ public class InputView {
                 System.out.println(e.getMessage());
             }
         }
+    }
+
+    public Map<String, Integer> askOrderMenus() {
+        System.out.println(ASK_ORDER_INFO);
+        Map<String, Integer> orderInfo = Arrays.stream(Console.readLine().split(COMMA))
+                .map(orderItem -> orderItem.split(HYPHEN))
+                .filter(parts -> parts.length == 2)
+                .collect(Collectors.toMap(
+                        parts -> parts[0],
+                        parts -> Integer.parseInt(parts[1]),
+                        Integer::sum));
+        return orderInfo;
     }
 
 }
