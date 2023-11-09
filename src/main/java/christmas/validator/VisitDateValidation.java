e3f0001be7974b4aca2d838e4f26833c9c54beab
hwan2-99 <thh1233123@gmail.com>
2023-11-09 18:47:22 +0900

    feat: 방문날짜 입력시 1~31 사이 값 아닐시 예외처리

--- src/main/java/christmas/validator/VisitDateValidation.java
+++ src/main/java/christmas/validator/VisitDateValidation.java
@@ -1,13 +1,22 @@
 package christmas.validator;
 
+import christmas.constant.Date;
+
 public class VisitDateValidation {
     public void validate(String inputValue){
         validateIncorrectInput(inputValue);
+        validateDateRange(inputValue);
     }
     private void validateIncorrectInput(String inputValue){
         try {
             Integer.parseInt(inputValue);
         }catch (NumberFormatException e){
+            throw new IllegalArgumentException();
+        }
+    }
+    private void validateDateRange(String inputValue){
+        int date = Integer.parseInt(inputValue);
+        if(date < Date.FIRST_DAY.getDate() || date > Date.LAST_DAY.getDate()){
             throw new IllegalArgumentException();
         }
     }
