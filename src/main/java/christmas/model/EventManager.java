2096666a61f97e5a0db7e3cb891b6b5a105af03a
hwan2-99 <thh1233123@gmail.com>
2023-11-12 22:37:22 +0900

    fix: 크리스마스 디데이 할인 조건 수정

--- src/main/java/christmas/model/EventManager.java
+++ src/main/java/christmas/model/EventManager.java
@@ -11,15 +11,17 @@ public class EventManager {
     private static final int YEAR = 2023;
     private static final int DECEMBER = 12;
     private static final int CHRISTMAS_DAY = 25;
+    private static final int EVENT_END_DATE = 26;
     private final Map<Integer, Integer> calender = new HashMap<>();
 
 
     public EventManager() {
         for (int i = Date.FIRST_DAY.getDate(); i <= CHRISTMAS_DAY; i++) {
             calender.put(i,
-                    Price.INITIAL_DISCOUNT_PRICE.getPrice() + Price.ADDITIONAL_DISCOUNT_PRICE.getPrice() * (i-Date.FIRST_DAY.getDate()));
+                    Price.INITIAL_DISCOUNT_PRICE.getPrice() + Price.ADDITIONAL_DISCOUNT_PRICE.getPrice() * (i
+                            - Date.FIRST_DAY.getDate()));
         }
-        for (int i = CHRISTMAS_DAY; i <= Date.LAST_DAY.getDate(); i++) {
+        for (int i = EVENT_END_DATE; i <= Date.LAST_DAY.getDate(); i++) {
             calender.put(i, Price.NONE.getPrice());
         }
     }
