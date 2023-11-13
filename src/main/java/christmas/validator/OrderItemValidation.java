c6efd8476631cebc6a5140fc7c18bf834ebb6794
hwan2-99 <thh1233123@gmail.com>
2023-11-13 20:13:47 +0900

    refactor: InputView 클래스의 메뉴 분리 메소드 검증클래스로 이동

--- src/main/java/christmas/validator/OrderItemValidation.java
+++ src/main/java/christmas/validator/OrderItemValidation.java
@@ -10,17 +10,33 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 public class OrderItemValidation {
+    private static final String COMMA = ",";
+    private static final String HYPHEN = "-";
     private static final int MIN_QUANTITY = 1;
     private static final int MAX_QUANTITY = 20;
     private final List<String> menus = (List<String>) Arrays.stream(Menu.values()).map(Menu::getName).toList();
 
-    public void validateSeparatedOrders(Map<String, Integer> orderInfo) {
+    public Map<String, Integer> validateSeparatedOrders(String inputValue) {
+        Map<String, Integer> orderInfo = separateOrders(inputValue);
         validateQuantity(orderInfo);
         validateMenuNames(orderInfo);
         validateMenuAllQuantity(orderInfo);
         validateDuplicateName(orderInfo);
+        return orderInfo;
     }
-
+    private Map<String, Integer> separateOrders(String inputValue) {
+        try {
+            return Arrays.stream(inputValue.split(COMMA)).map(orderItem -> orderItem.split(HYPHEN))
+                    .filter(parts -> parts.length == 2)
+                    .collect(Collectors.toMap(parts -> parts[0],
+                            parts -> Integer.parseInt(parts[1]),
+                            (existing, replacement) -> {
+                                throw new IllegalArgumentException(ErrorMessage.INCORRECT_ORDER_ERROR.getMessage());
+                            }));
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ErrorMessage.INCORRECT_ORDER_ERROR.getMessage());
+        }
+    }
     private void validateQuantity(Map<String, Integer> orderInfo) {
         for (int quantity : orderInfo.values()) {
             if (quantity < MIN_QUANTITY) {
--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,16 +1,11 @@
 package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import christmas.constant.message.ErrorMessage;
 import christmas.validator.OrderItemValidation;
 import christmas.validator.VisitDateValidation;
-import java.util.Arrays;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class InputView {
-    private static final String COMMA = ",";
-    private static final String HYPHEN = "-";
     private static final String ASK_VISIT_DATE_MESSAGE =
             "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.\n" + "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
     private static final String ASK_ORDER_INFO = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
@@ -35,26 +30,10 @@ public class InputView {
         System.out.println(ASK_ORDER_INFO);
         while (true) {
             try {
-                Map<String, Integer> orderInfo = separateOrders();
-                orderItemValidation.validateSeparatedOrders(orderInfo);
-                return orderInfo;
+                return orderItemValidation.validateSeparatedOrders(inputOrders());
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
-        }
-    }
-
-    private Map<String, Integer> separateOrders() {
-        try {
-            return Arrays.stream(inputOrders().split(COMMA)).map(orderItem -> orderItem.split(HYPHEN))
-                    .filter(parts -> parts.length == 2)
-                    .collect(Collectors.toMap(parts -> parts[0],
-                            parts -> Integer.parseInt(parts[1]),
-                            (existing, replacement) -> {
-                                throw new IllegalArgumentException(ErrorMessage.INCORRECT_ORDER_ERROR.getMessage());
-                            }));
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(ErrorMessage.INCORRECT_ORDER_ERROR.getMessage());
         }
     }
 
